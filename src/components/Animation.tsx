1db021b96d9bba9f5b50661ec8d14c5ceaef76a3
Dearth FuNk <dearthfunk@gmail.com>
2024-02-25 00:02:05 -0500

    animation now works with resizing, time to create something?

--- src/components/Animation.tsx
+++ src/components/Animation.tsx
@@ -1,78 +1,36 @@
-import { useRef, useEffect } from "react";
-import useWindowSize from "./UseWindowSize";
+import { useRef, useEffect, useState } from "react";
+import p5 from 'p5';
 
 export default function Animation() {
-  // window size ( width, height ) pass into animation for canvas resizing
-  const windowSize = useWindowSize();
-  const canvasRef = useRef<HTMLCanvasElement>(null);
-  const ctx = canvasRef.current?.getContext("2d");
-  const previousTimeRef = useRef(0);
-  const requestRef = useRef(0);
-  let size = 450;
-  let size2 = 440;
-  const animate = (time: number) => {
-    if (previousTimeRef.current !== undefined && ctx) {
-      // https://stackoverflow.com/questions/53310138/creating-a-draggable-and-scaleable-grid-in-html5
+  const [p5Canvas, setP5Canvas] = useState<p5 | null>(null);
+  const sketchRef = useRef<HTMLDivElement>(null);
 
-      //partial erase
-      var oldArray = ctx.getImageData(
-        0,
-        0,
-        ctx.canvas.width,
-        ctx.canvas.height
-      );
-      for (var d = 3; d < oldArray.data.length; d += 4) {
-        //count through only the alpha pixels
-        //dim it with some feedback, I'm using .9
-        oldArray.data[d] = Math.floor(oldArray.data[d] * 0.5);
+  useEffect( () => {
+    const initSketch = (p: any) => {
+      p.preload = () => {
+          //load images and shaders here
       }
-      ctx.putImageData(oldArray, 0, 0);
-
-      // render animation
-      ctx.beginPath();
-      ctx.arc(0, 0, size2, 0, 2 * Math.PI);
-      ctx.strokeStyle = "#FFBBBB";
-      ctx.stroke();
-      ctx.closePath();
-
-      ctx.beginPath();
-      ctx.arc(0, 0, size, 0, 2 * Math.PI);
-      ctx.strokeStyle = "#FFFFFF";
-      ctx.stroke();
-      ctx.closePath();
-
-      size = --size;
-      size2 = --size2;
-      if (size <= 250) {
-        size = 450;
+      p.windowResized = (a: any, b: any, c: any) => {
+          p.resizeCanvas(window.innerWidth, window.innerHeight);
       }
-      if (size2 <= 250) {
-        size2 = 440;
+      p.setup = () => {
+          //setup canvas and init values here
+          p.createCanvas(window.innerWidth, window.innerWidth);
       }
-    }
+  
+      p.draw = () => {
+          //update canvas here
+          p.background(p.frameCount % 255)
+      }
+    };
+    const canvas = new p5(initSketch, sketchRef.current as HTMLElement);
+    setP5Canvas(canvas);
+    return () => { canvas?.remove(); }
+  }, []);
 
-    previousTimeRef.current = time;
-    requestRef.current = requestAnimationFrame(animate);
-  };
+  if (!p5Canvas) {
+    return null;
+  }
 
-  useEffect(() => {
-    const ctx = canvasRef.current?.getContext("2d");
-    if (ctx) {
-      ctx.translate(windowSize.width / 2, windowSize.height / 2);
-      ctx.globalCompositeOperation = "hue";
-    }
-  }, [windowSize]);
-
-  useEffect(() => {
-    requestRef.current = requestAnimationFrame(animate);
-    return () => cancelAnimationFrame(requestRef.current);
-  });
-
-  return (
-    <canvas
-      ref={canvasRef}
-      width={windowSize.width + "px"}
-      height={windowSize.height + "px"}
-    ></canvas>
-  );
+  return <div className="mySketch" ref={sketchRef} />;
 }
